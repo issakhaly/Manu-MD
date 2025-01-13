f4f6123a587f8eccdcdaa371fc08364e920b5dc2
issakhaly <157059290+issakhaly@users.noreply.github.com>
2025-01-13 22:19:35 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
-GITHUB_USERNAME: process.env.GITHUB_USERNAME || "put your github usearname",
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "put your github auth token without ghp_",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&WR0WCbTB#BQKqRqESUF2ylkW9aUtJPejW7dW0UfaULtzsryhwI3w",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME || "issakhaly",
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "SdRK51cNJKFQqph9Oatg9FdeisIOLy2pYVmU",
 };
